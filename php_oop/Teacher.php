470b68d1f1ae4253173dfe8c021a2249aa83a536
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-01-27 21:50:16 +0530

    13. What Is Class

---
+++ php_oop/Teacher.php
@@ -0,0 +1,10 @@
+<?php 
+class Teacher{
+    // public means variables can be accessed outside and inside freely
+    // outside use an object
+    public $name;
+    public $age;
+    public $address;
+    
+
+}
