d2e28a6078ad73e4f3d6c16ce0abf05def31469c
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-01-27 22:01:09 +0530

    14. Class Methods & Class Objects

--- php_oop/Teacher.php
+++ php_oop/Teacher.php
@@ -5,6 +5,24 @@ class Teacher{
     public $name;
     public $age;
     public $address;
-    
 
+    public function teacherName(){
+        return ($this->name) ? $this->name : "Name Not Set";
+    }
+    public function teacherAddress(){
+        return ($this->address) ? $this->address : "Address Not Set";
+    }
 }
+$t1 = new Teacher();
+$t1->name = "Some Teacher Name";
+echo $t1->teacherName();
+echo PHP_EOL;
+echo $t1->teacherAddress();
+echo PHP_EOL;
+$t1->address = "Gujarat, India";
+echo $t1->teacherAddress();
+echo PHP_EOL."New Object Created".PHP_EOL;
+$t2 = new Teacher();
+echo $t2->teacherName();
+echo PHP_EOL;
+echo $t2->teacherAddress();
