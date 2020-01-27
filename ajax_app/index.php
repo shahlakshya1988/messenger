ba1aa1b0ca8604f20cd1b08c6873a4f19fb581fa
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-01-27 08:36:30 +0530

    6. Create Index File & Link CSS File & jQuery File, Intial Setup

---
+++ ajax_app/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Basics</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <form action="">
+            <button class="btn btn-success" type="button" id="btn">Enter</button>
+        </form>
+    </div>
+    
+    <script src="assets/js/jquery.min.js"></script>    
+    <script src="assets/js/bootstrap.bundle.min.js"></script>    
+    <script>
+        $(document).ready(function(){
+            alert("Jquery Working");
+        });
+    </script>
+</body>
+</html>
