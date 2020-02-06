4c2443a3bedbc1816456f177960750ca82161126
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-06 18:12:45 +0530

    Placing Assets

---
+++ signup.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Create New Account</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+</head>
+<body>
+	
+</body>
+</html>
