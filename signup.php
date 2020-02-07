62e989c89d92b6d7e71b25cda4b19dd132095c8a
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-07 10:41:53 +0530

    29. What Is CSS Variable?

--- signup.php
+++ signup.php
@@ -2,13 +2,16 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width;initial-scale=1.0,shrink-to-fit=no">
 	<title>Create New Account</title>
-	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-	<!-- font-family: 'Poppins', sans-serif; -->
+	<!-- font-family: 'Poppins', sans-serif; -->	
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	
+	<div class="box box1"></div>
+	<div class="box box2"></div>
+	<div class="box box3"></div>
 </body>
 </html>
