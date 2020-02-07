af3c316b2b3a590e1c74fe0a35917d22894d0ec7
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-07 10:29:45 +0530

    28. Project Setup

--- signup.php
+++ signup.php
@@ -5,6 +5,8 @@
 	<title>Create New Account</title>
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<!-- font-family: 'Poppins', sans-serif; -->
 </head>
 <body>
 	
