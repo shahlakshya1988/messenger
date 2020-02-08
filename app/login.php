145fc3d0788d41e8acb0c221f9c14ee93ee41e0f
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-08 13:21:11 +0530

    36. Design Signup & Login Form Part5

---
+++ app/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width;initial-scale=1.0,shrink-to-fit=no">
+	<title>User Login</title>
+	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<!-- font-family: 'Poppins', sans-serif; -->	
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="signup-container">
+		<div class="account-left">
+			<div class="account-text">
+				<h1>Lets Chat</h1>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, autem dicta quae saepe provident delectus, eum reiciendis obcaecati quis ab ex excepturi! Nobis fugiat eum porro iure rem itaque quam maiores in magni quo praesentium optio quis distinctio laudantium nulla dolore, dolorem, voluptates ipsam hic iusto? Aut nostrum perspiciatis vitae blanditiis! Aliquam quas, harum totam dolore ex cum excepturi placeat voluptatum sunt, hic iste consequuntur expedita delectus nesciunt tenetur rem deleniti quae libero assumenda perspiciatis! Expedita eligendi qui minima animi quas ipsum totam dolorem ut. Delectus temporibus voluptate totam, expedita magnam sequi molestiae illum amet, possimus quos eum error. Odio!</p>
+			</div>
+			<!-- div.account-text -->
+		</div>
+		<!-- div.account-left -->
+		<div class="account-right">
+			<!-- <h1>Account Right</h1> -->
+			<div class="form-area">
+				<form action="" method="POST" enctype="multipart/form-data">
+					<div class="group">
+						<h2 class="form-heading">User Login</h2>
+					</div>
+					<!-- div.group -->
+					
+					<div class="group">
+						<input type="email" name="email" id="email" class="control" placeholder="Enter Email Address">
+					</div>
+					<!-- div.group -->
+					<div class="group">
+						<input type="password" name="password" id="password" class="control" placeholder="Enter Password">
+					</div>
+					<!-- div.group -->
+					
+					<div class="group">
+						<input type="submit" name="login" id="login" class="btn account-btn" value="User Login">
+					</div>
+					<!-- div.group -->
+					<div class="group">
+						<a href="signup.php" class="link">Signup</a>
+					</div>
+					<!-- div.group -->
+				</form>
+			</div>
+			<!-- div.form-area -->
+		</div>
+		<!-- div.account-right -->
+	</div>
+	<!-- div.signup-container -->
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/formlabel.js"></script>
+</body>
+</html>
