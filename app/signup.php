db2cbe4f6553bf777ed66a464fde769e39859c99
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-08 13:11:48 +0530

    35. Design Signup & Login Form Part4

--- app/signup.php
+++ app/signup.php
@@ -12,7 +12,11 @@
 <body>
 	<div class="signup-container">
 		<div class="account-left">
-			<h1>Account Left</h1>
+			<div class="account-text">
+				<h1>Lets Chat</h1>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, autem dicta quae saepe provident delectus, eum reiciendis obcaecati quis ab ex excepturi! Nobis fugiat eum porro iure rem itaque quam maiores in magni quo praesentium optio quis distinctio laudantium nulla dolore, dolorem, voluptates ipsam hic iusto? Aut nostrum perspiciatis vitae blanditiis! Aliquam quas, harum totam dolore ex cum excepturi placeat voluptatum sunt, hic iste consequuntur expedita delectus nesciunt tenetur rem deleniti quae libero assumenda perspiciatis! Expedita eligendi qui minima animi quas ipsum totam dolorem ut. Delectus temporibus voluptate totam, expedita magnam sequi molestiae illum amet, possimus quos eum error. Odio!</p>
+			</div>
+			<!-- div.account-text -->
 		</div>
 		<!-- div.account-left -->
 		<div class="account-right">
