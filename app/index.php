57713515ce1c1662eeaba1affb70e43d400900d8
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-08 16:06:09 +0530

    38. Create Blue Print

---
+++ app/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width;initial-scale=1.0,shrink-to-fit=no">
+	<title>Home</title>
+	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<!-- font-family: 'Poppins', sans-serif; -->	
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav id="nav">
+    
+    </nav>
+    <!-- nav#nav -->
+    <div class="chat-container">
+        <section id="sidebar">
+        
+        </section>
+        <!-- section#sidebar -->
+        <section id="right-area">
+            <div class="messages">
+            
+            </div>
+            <!-- div.messages -->
+            <div class="chat-form">
+            
+            </div>
+            <!-- div.chat-form -->
+        </section>
+        <!-- section#right-area -->
+    </div>
+    <!-- div.chat container -->
+</body>
+</html>
