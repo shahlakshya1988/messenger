491aaeb8aed0094f76df79b985aa31c077123ca7
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-11 17:37:09 +0530

    41. Build & Design Messages Section

--- app/index.php
+++ app/index.php
@@ -28,7 +28,56 @@
         <!-- section#sidebar -->
         <section id="right-area">
             <div class="messages">
-            
+                <div class="left-message common-margin">
+                    <div class="sender-img-block">
+                        <img src="assets/img/pro3.jpg" class="sender-img" alt="">
+                    </div>
+                    <!-- div.sender-img-block -->
+                    <div class="left-msg-area">
+                        <div class="user-name-date">
+                            <span class="sender-name">
+                                Shakil Khan
+                            </span>
+                            <!-- span.sender-name -->
+                            <span class="date-time">
+                                1 Day Ago
+                            </span>
+                            <!-- span.date-time -->
+                        </div>
+                        <!-- div.user-name-date -->
+                        <div class="left-msg">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate dolorum consequuntur eveniet porro laborum incidunt adipisci, ullam, omnis obcaecati, reprehenderit nesciunt commodi totam minus nihil. Quisquam odio fugiat eveniet recusandae cum praesentium error accusamus totam soluta tempore, magnam distinctio. Fuga deleniti assumenda ratione voluptate vero magni repudiandae tempora suscipit iste dolorem. Autem ullam eum in dicta facilis perferendis aliquam dolorem.</p>
+                        </div>
+                        <!-- div.left-msg -->
+                    </div>
+                    <!-- div.left-msg-area -->
+                </div>
+                <!-- div.left-message -->
+                <div class="left-message common-margin">
+                    <div class="sender-img-block">
+                        <img src="assets/img/pro3.jpg" class="sender-img" alt="">
+                    </div>
+                    <!-- div.sender-img-block -->
+                    <div class="left-msg-area">
+                        <div class="user-name-date">
+                            <span class="sender-name">
+                                Shakil Khan
+                            </span>
+                            <!-- span.sender-name -->
+                            <span class="date-time">
+                                1 Day Ago
+                            </span>
+                            <!-- span.date-time -->
+                        </div>
+                        <!-- div.user-name-date -->
+                        <div class="left-msg">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate dolorum consequuntur eveniet porro laborum incidunt adipisci, ullam, omnis obcaecati, reprehenderit nesciunt commodi totam minus nihil. Quisquam odio fugiat eveniet recusandae cum praesentium error accusamus totam soluta tempore, magnam distinctio. Fuga deleniti assumenda ratione voluptate vero magni repudiandae tempora suscipit iste dolorem. Autem ullam eum in dicta facilis perferendis aliquam dolorem.</p>
+                        </div>
+                        <!-- div.left-msg -->
+                    </div>
+                    <!-- div.left-msg-area -->
+                </div>
+                <!-- div.left-message -->
             </div>
             <!-- div.messages -->
             <div class="chat-form">
