1936ecb50055f119088644fc1a749e29d7797f9f
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-13 15:36:42 +0530

    Corrected the Meta View Port

--- app/change_name.php
+++ app/change_name.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width;initial-scale=1.0,shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no">
 	<title>Home</title>
 	<link rel="stylesheet" href="assets/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
