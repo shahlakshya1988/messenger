9f1756fb1c13b788a0ae72951c230ef108d65212
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-13 14:29:41 +0530

    49. Add Media Query Part2

--- app/index.php
+++ app/index.php
@@ -153,5 +153,13 @@
         <!-- section#right-area -->
     </div>
     <!-- div.chat container -->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".custom-bars").click(function(){
+                $("#sidebar").slideToggle();
+            });
+        });
+    </script>
 </body>
 </html>
