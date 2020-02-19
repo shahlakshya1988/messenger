2ee24a19615c6240f9fb2673393066e47ddcfe80
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-19 12:33:42 +0530

    87. Show Messages

--- app/ajax/show_messages.php
+++ app/ajax/show_messages.php
@@ -1,6 +1,31 @@
 <?php require __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."init.php"; 
 $obj = new base_class();
 if(isset($_GET["message"]) && !empty(trim($obj->security($_GET["message"])))){
-    echo "<pre>",print_r($_GET),"</pre>";
+    //echo "<pre>",print_r($_GET),"</pre>";
+    /** we will fetch clean_message_id */
+    $query="SELECT `clean_message_id` from `clean` where `clean_user_id` = :clean_user_id";
+    $param = array(":clean_user_id"=>$_SESSION["user_id"]);
+    $obj->normalQuery($query,$param);
+    $result = $obj->fetch_single();
+    $clean_message_id = $result->clean_message_id;
+    // echo "<pre>",print_r($result),"</pre>";
+$fetch_last_message_id_query = "SELECT `message_id` FROM `messages`  order by `message_id` DESC LIMIT 1";
+$obj->normalQuery($fetch_last_message_id_query);
+$result_last_message_id = $obj->fetch_single();
+$last_message_id = $result_last_message_id->message_id;
+//echo $last_message_id;
+    /** we will fetch clean_message_id */
+
+    /*** we will fetch messages between those ids clean_message_id and last_message_id */
+    $query = "SELECT * FROM `messages` inner join `users` on `messages`.`user_id` = `users`.`id` where `messages`.`message_id` BETWEEN :clean_message_id and :last_message_id ORDER BY `messages`.`message_id` ASC ";
+    $param = array(":clean_message_id"=>$clean_message_id,":last_message_id"=>$last_message_id);
+    $obj->normalQuery($query,$param);
+    $result_msg = $obj->fetch_all();
+    //echo "<pre>",print_r($result_msg),"</pre>";
+    foreach($result_msg as $informations){
+        print_r($informations);
+        echo "<br><br>";
+    }
+    /*** we will fetch messages between those ids clean_message_id and last_message_id */
 }
 ?>
--- app/sql_db/messenger.sql
+++ app/sql_db/messenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 19, 2020 at 05:34 AM
+-- Generation Time: Feb 19, 2020 at 06:58 AM
 -- Server version: 5.7.28-log
 -- PHP Version: 5.6.40
 
@@ -34,7 +34,15 @@ CREATE TABLE IF NOT EXISTS `clean` (
   `clean_message_id` int(255) NOT NULL,
   `clean_user_id` int(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `clean`
+--
+
+INSERT INTO `clean` (`id`, `clean_message_id`, `clean_user_id`) VALUES
+(1, 4, 1),
+(2, 7, 2);
 
 -- --------------------------------------------------------
 
@@ -51,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `msg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`message_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `messages`
@@ -59,7 +67,14 @@ CREATE TABLE IF NOT EXISTS `messages` (
 
 INSERT INTO `messages` (`message_id`, `message`, `msg_type`, `user_id`, `msg_time`) VALUES
 (2, '15e4a8cc11c5908.54524634.jpg', 'jpg', 1, '2020-02-17 12:53:21'),
-(3, 'assets/emoji/emoji16.png', 'emoji', 1, '2020-02-18 06:00:46');
+(3, 'assets/emoji/emoji16.png', 'emoji', 1, '2020-02-18 06:00:46'),
+(4, 'Hello', 'text', 1, '2020-02-19 06:33:58'),
+(5, 'hiii', 'text', 1, '2020-02-19 06:34:00'),
+(6, 'i am new user', 'text', 1, '2020-02-19 06:34:03'),
+(7, 'Messages after hello1 login', 'text', 1, '2020-02-19 06:36:10'),
+(8, 'I am a new user', 'text', 2, '2020-02-19 06:40:04'),
+(9, 'hello1', 'text', 2, '2020-02-19 06:40:05'),
+(10, 'Checking the code', 'text', 1, '2020-02-19 06:57:45');
 
 -- --------------------------------------------------------
 
@@ -77,14 +92,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `status` int(11) NOT NULL DEFAULT '0' COMMENT 'if user is online then it will store 1 otherwise 0',
   `clean_status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `image`, `status`, `clean_status`) VALUES
-(1, 'hello', 'hello@example.com', '$2y$10$ZH.wzsUxQBE2eAEu/S8jTugFEYLKVZaPHrs.dZ.WSncBnmaMR1axm', '5e4a54212633f3.63902710.png', 0, 0);
+(1, 'hello', 'hello@example.com', '$2y$10$ZH.wzsUxQBE2eAEu/S8jTugFEYLKVZaPHrs.dZ.WSncBnmaMR1axm', '5e4a54212633f3.63902710.png', 1, 1),
+(2, 'hello1', 'hello1@example.com', '$2y$10$Zhn3AjPfsQhAYkta4k06/.z1evNixudXviQlAK0NVEXRQcniqln4G', '5e4cd735b33959.61866800.png', 1, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
