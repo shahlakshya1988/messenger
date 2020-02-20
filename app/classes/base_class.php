19ebfc06a61601a1eee757d7b056752ee262e74a
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-20 12:59:08 +0530

    94. Add Time Ago Feature

--- app/classes/base_class.php
+++ app/classes/base_class.php
@@ -29,11 +29,24 @@ class base_class extends db
     {
         return trim(strip_tags($data)); // remove all the html 
     }
-    public function fetch_single(){
+    public function fetch_single()
+    {
         return $this->query->fetch(PDO::FETCH_OBJ);
     }
     public function createSession($session_name, $session_value)
     {
         $_SESSION[$session_name] = $session_value;
+    }
+    public function time_age($db_message_time)
+    {
+        $db_time = strtotime($db_message_time);
+        $current_time = time();
+        $seconds = $current_time - $db_time;
+        $minutes = floor($seconds / 60);
+        $hours = floor($seconds/(60*60));// 60->seconds in one minute 60->minues in 1 hour
+        $days =  floor($seconds/(60*60*24)); // 24 hours in one day
+        $weeks = floor($seconds/(7*60*60*24)); // 7 days in one week
+        $months = floor($seconds/(30*60*60*24)); // 30 days in one month
+        $years = floor($seconds/(365*60*60*24)); // 365 days in one month
     }
 }
