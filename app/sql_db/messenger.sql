0b6cbf043edc8e4a130b67c4882de1619e877030
Lakshya Shah <shahlakshya1988@gmail.com>
2020-02-20 11:32:00 +0530

    93. Show Messages Part7

--- app/sql_db/messenger.sql
+++ app/sql_db/messenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 19, 2020 at 12:06 PM
+-- Generation Time: Feb 20, 2020 at 05:55 AM
 -- Server version: 5.7.28-log
 -- PHP Version: 5.6.40
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `clean` (
   `clean_message_id` int(255) NOT NULL,
   `clean_user_id` int(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `clean`
@@ -42,7 +42,8 @@ CREATE TABLE IF NOT EXISTS `clean` (
 
 INSERT INTO `clean` (`id`, `clean_message_id`, `clean_user_id`) VALUES
 (1, 4, 1),
-(2, 7, 2);
+(2, 7, 2),
+(3, 22, 3);
 
 -- --------------------------------------------------------
 
@@ -59,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `msg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`message_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `messages`
@@ -77,7 +78,16 @@ INSERT INTO `messages` (`message_id`, `message`, `msg_type`, `user_id`, `msg_tim
 (10, 'Checking the code', 'text', 1, '2020-02-19 06:57:45'),
 (11, 'hello Welcome', 'text', 1, '2020-02-19 11:57:14'),
 (12, 'assets/emoji/emoji6.png', 'emoji', 1, '2020-02-19 11:58:01'),
-(13, '15e4d243a052881.07132283.jpg', 'jpg', 1, '2020-02-19 12:04:10');
+(13, '15e4d243a052881.07132283.jpg', 'jpg', 1, '2020-02-19 12:04:10'),
+(14, '25e4e17f69ebb89.50303045.zip', 'zip', 2, '2020-02-20 05:24:06'),
+(15, '15e4e192eb02be4.00312385.pdf', 'pdf', 1, '2020-02-20 05:29:18'),
+(16, '15e4e1b5c5dace7.82850712.docx', 'docx', 1, '2020-02-20 05:38:36'),
+(17, '15e4e1b6bb74bc1.25245927.xlsx', 'xlsx', 1, '2020-02-20 05:38:51'),
+(18, '15e4e1b709c9e18.53012693.docx', 'docx', 1, '2020-02-20 05:38:56'),
+(19, '25e4e1b8b305eb1.98327303.docx', 'docx', 2, '2020-02-20 05:39:23'),
+(20, '25e4e1b94239407.03001295.xlsx', 'xlsx', 2, '2020-02-20 05:39:32'),
+(21, '15e4e1bb9df5876.81802107.zip', 'zip', 1, '2020-02-20 05:40:09'),
+(22, 'hello', 'text', 3, '2020-02-20 05:52:32');
 
 -- --------------------------------------------------------
 
@@ -95,15 +105,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `status` int(11) NOT NULL DEFAULT '0' COMMENT 'if user is online then it will store 1 otherwise 0',
   `clean_status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `image`, `status`, `clean_status`) VALUES
-(1, 'hello', 'hello@example.com', '$2y$10$ZH.wzsUxQBE2eAEu/S8jTugFEYLKVZaPHrs.dZ.WSncBnmaMR1axm', '5e4a54212633f3.63902710.png', 1, 1),
-(2, 'hello1', 'hello1@example.com', '$2y$10$Zhn3AjPfsQhAYkta4k06/.z1evNixudXviQlAK0NVEXRQcniqln4G', '5e4cd735b33959.61866800.png', 1, 1);
+(1, 'hello', 'hello@example.com', '$2y$10$ZH.wzsUxQBE2eAEu/S8jTugFEYLKVZaPHrs.dZ.WSncBnmaMR1axm', '5e4a54212633f3.63902710.png', 0, 1),
+(2, 'hello1', 'hello1@example.com', '$2y$10$Zhn3AjPfsQhAYkta4k06/.z1evNixudXviQlAK0NVEXRQcniqln4G', '5e4cd735b33959.61866800.png', 1, 1),
+(3, 'hello2@example.com', 'hello2@example.com', '$2y$10$gqU6pMCZLdYda9RO38uOaOiIhBnRNwsBeZnAel5sEFWRw0R7J0h/K', '5e4e1e93a03551.20743619.png', 1, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
